1a3f93631f532e441cdf5d5ea2dabc5d1ec1fb8a
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2022-12-20 19:52:27 +0300

    Начал нормальную реализацию класса TImage. Убрал всякий мусор из трека

--- map/models/timage.h
+++ map/models/timage.h
@@ -9,7 +9,21 @@ class TImage : public QObject
 {
     Q_OBJECT
 public:
-    explicit TImage(QObject *parent = nullptr);
+    explicit TImage(QObject *parent = nullptr, QByteArray data = QByteArray(), QString filePath = "", ImageMode mode = ImageMode::Raw, qreal thetaAzimuthCorrection = 0);
+
+    QString cachedJPEGfilename = ""; //check
+    QString base64 = "";
+    qreal realWidth = 0;
+    qreal realHeight = 0;
+    
+    bool checksumMatch = false; //check
+    
+    qint32 index = 0; //check
+
+    qreal horizontalCorrection = 0;
+    qreal verticalCorrection = 0;
+    qreal angleCorrection = 0;
+    qreal scaleCorrection = 0;
 
     struct Metadata
     {
@@ -26,27 +40,6 @@ public:
         float thetaAzimuth = 0;
         uint32_t checksum = 0;
     };
-
-    QString cachedJPEGfilename = ""; //check
-    QString base64 = "";
-    qreal realWidth = 0;
-    qreal realHeight = 0;
-    
-    bool checksumMatch = false; //check
-    
-    qint32 index = 0; //check
-    qreal opacity = 1;
-    bool visible = true;
-
-    qreal horizontalCorrection = 0;
-    qreal verticalCorrection = 0;
-    qreal angleCorrection = 0;
-    qreal scaleCorrection = 0;
-    
-    bool drawSARmarker = true;
-    QString SARMarkerName = "";
-    QColor SARMarkerColor = Qt::white;
-
     struct Interface
     {
         QString latitude = "";
@@ -68,7 +61,19 @@ public:
     Interface gui; //check
     QImage image; //check
 
+private:
+    bool decode();
+    void assignUIStrings();
+    QImage dataToQImage(QByteArray data);
+    QString QImageToBase64(QImage image);
+    QImage enableAlphaChannel(QImage image);
+    QImage applyAlphaMask(QImage image, qreal width, qreal height, qreal thetaAzimuthCorrection, qreal x0);
+
+//
+
+
 signals:
+    void exception(QString exceptionCode);
 };
 
 #endif // TIMAGE_H

f9ce4c4c45995eaa77383910d0100904fae3a30c
Рязанцев Дмитрий <ryazancev_dl@uav.radar-mms.com>
2023-01-17 15:43:08 +0300

    Добавил сигналы и сеттеры к проперти
    Signed-off-by: Рязанцев Дмитрий <ryazancev_dl@uav.radar-mms.com>

--- gui/thememanager.cpp
+++ gui/thememanager.cpp
@@ -65,50 +65,48 @@ ThemeManager::ThemeManager(QObject *parent, short themeInitializer) : QObject{pa
     }
 }
 
-QColor ThemeManager::getPrimaryDarkest()        { return prm_darkest; }
-QColor ThemeManager::getPrimaryDarker()         { return prm_darker; }
-QColor ThemeManager::getPrimaryDark()           { return prm_dark; }
-QColor ThemeManager::getPrimaryLight()          { return prm_light; }
-QColor ThemeManager::getPrimaryLighter()        { return prm_lighter; }
-QColor ThemeManager::getPrimaryLightest()       { return prm_lightest; }
-
-QColor ThemeManager::getTextFaded()             { return txt_faded; }
-QColor ThemeManager::getTextColored()           { return txt_colored; }
-QColor ThemeManager::getTextWhite()             { return txt_white; }
-
-QColor ThemeManager::getWarningDarkest()        { return wrn_darkest; }
-QColor ThemeManager::getWarningDarker()         { return wrn_darker; }
-QColor ThemeManager::getWarningDark()           { return wrn_dark; }
-QColor ThemeManager::getWarningLight()          { return wrn_light; }
-QColor ThemeManager::getWarningLighter()        { return wrn_lighter; }
-QColor ThemeManager::getWarningLightest()       { return wrn_lightest; }
-
-QColor ThemeManager::getAccentDarkest()         { return acc_darkest; }
-QColor ThemeManager::getAccentDarker()          { return acc_darker; }
-QColor ThemeManager::getAccentDark()            { return acc_dark; }
-QColor ThemeManager::getAccentLight()           { return acc_light; }
-QColor ThemeManager::getAccentLighter()         { return acc_lighter; }
-QColor ThemeManager::getAccentLightest()        { return acc_lightest; }
-
-QColor ThemeManager::getInfoDarkest()           { return inf_darkest; }
-QColor ThemeManager::getInfoDarker()            { return inf_darker; }
-QColor ThemeManager::getInfoDark()              { return inf_dark; }
-QColor ThemeManager::getInfoLight()             { return inf_light; }
-QColor ThemeManager::getInfoLighter()           { return inf_lighter; }
-QColor ThemeManager::getInfoLightest()          { return inf_lightest; }
-
-QColor ThemeManager::getErrorDarkest()          { return err_darkest; }
-QColor ThemeManager::getErrorDarker()           { return err_darker; }
-QColor ThemeManager::getErrorDark()             { return err_dark; }
-QColor ThemeManager::getErrorLight()            { return err_light; }
-QColor ThemeManager::getErrorLighter()          { return err_lighter; }
-QColor ThemeManager::getErrorLightest()         { return err_lightest; }
-
-QColor ThemeManager::getSuccessDarkest()        { return scs_darkest; }
-QColor ThemeManager::getSuccessDarker()         { return scs_darker; }
-QColor ThemeManager::getSuccessDark()           { return scs_dark; }
-QColor ThemeManager::getSuccessLight()          { return scs_light; }
-QColor ThemeManager::getSuccessLighter()        { return scs_lighter; }
-QColor ThemeManager::getSuccessLightest()       { return scs_lightest; }
-
-QColor ThemeManager::getMiscBrown()             { return msc_brown; }
+QColor ThemeManager::getPrimaryDarkest(){return prm_darkest;}void ThemeManager::setPrimaryDarkest(QColor color){if(color==prm_darkest)return;prm_darkest=color;
+emit primaryDarkestChanged();}QColor ThemeManager::getPrimaryDarker(){return prm_darker;}void ThemeManager::setPrimaryDarker(QColor color){if(color==prm_darker)
+return;prm_darker=color;emit primaryDarkerChanged();}QColor ThemeManager::getPrimaryDark(){return prm_dark;}void ThemeManager::setPrimaryDark(QColor color){if 
+(color==prm_dark)return;prm_dark=color;emit primaryDarkChanged();}QColor ThemeManager::getPrimaryLight(){return prm_light;}void ThemeManager::setPrimaryLight
+(QColor color){if(color==prm_light)return;prm_light=color;emit primaryLightChanged();}QColor ThemeManager::getPrimaryLighter(){return prm_lighter;}void 
+ThemeManager::setPrimaryLighter(QColor color){if(color==prm_lighter)return;prm_lighter=color;emit primaryLighterChanged();}QColor ThemeManager::getPrimaryLightest()
+{return prm_lightest;}void ThemeManager::setPrimaryLightest(QColor color){if(color==prm_lightest)return;prm_lightest=color;emit primaryLightestChanged();}QColor 
+ThemeManager::getTextFaded(){return txt_faded;}void ThemeManager::setTextFaded(QColor color){if(color==txt_faded)return;txt_faded=color;emit textFadedChanged();}
+QColor ThemeManager::getTextColored(){return txt_colored;}void ThemeManager::setTextColored(QColor color){if(color==txt_colored)return;txt_colored=color;emit 
+textColoredChanged();}QColor ThemeManager::getTextWhite(){return txt_white;}void ThemeManager::setTextWhite(QColor color){if(color==txt_white)return;txt_white=color;
+emit textWhiteChanged();}QColor ThemeManager::getWarningDarkest(){return wrn_darkest;}void ThemeManager::setWarningDarkest(QColor color){if(color==prm_darker)return;
+prm_darker=color;emit warningDarkestChanged();}QColor ThemeManager::getWarningDarker(){return wrn_darker;}void ThemeManager::setWarningDarker(QColor color){if
+(color==prm_darker)return;prm_darker=color;emit warningDarkerChanged();}QColor ThemeManager::getWarningDark(){return wrn_dark;}void ThemeManager::setWarningDark(
+QColor color){if(color==prm_darker)return;prm_darker=color;emit warningDarkChanged();}QColor ThemeManager::getWarningLight(){return wrn_light;}void ThemeManager::
+setWarningLight(QColor color){if(color==prm_darker)return;prm_darker=color;emit warningLightChanged();}QColor ThemeManager::getWarningLighter(){return wrn_lighter;}
+void ThemeManager::setWarningLighter(QColor color){if(color==prm_darker)return;prm_darker=color;emit warningLighterChanged();}QColor ThemeManager::getWarningLightest()
+{return wrn_lightest;}void ThemeManager::setWarningLightest(QColor color){if(color==prm_darker)return;prm_darker=color;emit warningLightestChanged();}QColor 
+ThemeManager::getAccentDarkest(){return acc_darkest;}void ThemeManager::setAccentDarkest(QColor color){if(color==prm_darker)return;prm_darker=color;emit 
+accentDarkestChanged();}QColor ThemeManager::getAccentDarker(){return acc_darker;}void ThemeManager::setAccentDarker(QColor color){if(color==prm_darker)return;
+prm_darker=color;emit accentDarkerChanged();}QColor ThemeManager::getAccentDark(){return acc_dark;}void ThemeManager::setAccentDark(QColor color){if(color==prm_darker)
+return;prm_darker=color;emit accentDarkChanged();}QColor ThemeManager::getAccentLight(){return acc_light;}void ThemeManager::setAccentLight(QColor color){if(color==
+prm_darker)return;prm_darker=color;emit accentLightChanged();}QColor ThemeManager::getAccentLighter(){return acc_lighter;}void ThemeManager::setAccentLighter(QColor 
+color){if(color==prm_darker)return;prm_darker=color;emit accentLighterChanged();}QColor ThemeManager::getAccentLightest(){return acc_lightest;}void ThemeManager::
+setAccentLightest(QColor color){if(color==prm_darker)return;prm_darker=color;emit accentLightestChanged();}QColor ThemeManager::getInfoDarkest(){return inf_darkest;}
+void ThemeManager::setInfoDarkest(QColor color){if(color==prm_darker)return;prm_darker=color;emit infoDarkestChanged();}QColor ThemeManager::getInfoDarker(){return 
+inf_darker;}void ThemeManager::setInfoDarker(QColor color){if(color==prm_darker)return;prm_darker=color;emit infoDarkerChanged();}QColor ThemeManager::getInfoDark()
+{return inf_dark;}void ThemeManager::setInfoDark(QColor color){if(color==prm_darker)return;prm_darker=color;emit infoDarkChanged();}QColor ThemeManager::getInfoLight()
+{return inf_light;}void ThemeManager::setInfoLight(QColor color){if(color==prm_darker)return;prm_darker=color;emit infoLightChanged();}QColor ThemeManager::
+getInfoLighter(){return inf_lighter; } void ThemeManager::setInfoLighter(QColor color){if(color==prm_darker)return;prm_darker=color;emit infoLighterChanged();}QColor 
+ThemeManager::getInfoLightest(){return inf_lightest; } void ThemeManager::setInfoLightest(QColor color){if(color==prm_darker)return;prm_darker=color;emit 
+infoLightestChanged();}QColor ThemeManager::getErrorDarkest(){return err_darkest;}void ThemeManager::setErrorDarkest(QColor color){if(color==prm_darker)return;
+prm_darker=color;emit errorDarkestChanged();}QColor ThemeManager::getErrorDarker(){return err_darker;}void ThemeManager::setErrorDarker(QColor color){if(color==
+prm_darker)return;prm_darker=color;emit errorDarkerChanged();}QColor ThemeManager::getErrorDark(){return err_dark;}void ThemeManager::setErrorDark(QColor color){if(
+color==prm_darker)return;prm_darker=color;emit errorDarkChanged();}QColor ThemeManager::getErrorLight(){return err_light;}void ThemeManager::setErrorLight(QColor color)
+{if(color==prm_darker)return;prm_darker=color;emit errorLightChanged();}QColor ThemeManager::getErrorLighter(){return err_lighter;}void ThemeManager::setErrorLighter(
+QColor color){if(color==prm_darker)return;prm_darker=color;emit errorLighterChanged();}QColor ThemeManager::getErrorLightest(){return err_lightest;}void ThemeManager::
+setErrorLightest(QColor color){if(color==prm_darker)return;prm_darker=color;emit errorLightestChanged();}QColor ThemeManager::getSuccessDarkest(){return scs_darkest;} 
+void ThemeManager::setSuccessDarkest(QColor color){if(color==prm_darker)return;prm_darker=color;emit successDarkestChanged();}QColor ThemeManager::getSuccessDarker()
+{return scs_darker;}void ThemeManager::setSuccessDarker(QColor color){if(color==prm_darker)return;prm_darker=color;emit successDarkerChanged();}QColor ThemeManager::
+getSuccessDark(){return scs_dark;}void ThemeManager::setSuccessDark(QColor color){if(color==prm_darker)return;prm_darker=color;emit successDarkChanged();}QColor 
+ThemeManager::getSuccessLight(){return scs_light;}void ThemeManager::setSuccessLight(QColor color){if(color==prm_darker)return;prm_darker=color;emit successLightChanged
+();}QColor ThemeManager::getSuccessLighter(){return scs_lighter;}void ThemeManager::setSuccessLighter(QColor color){if(color==prm_darker)return;prm_darker=color;emit 
+successLighterChanged();}QColor ThemeManager::getSuccessLightest(){return scs_lightest;}void ThemeManager::setSuccessLightest(QColor color){if(color==prm_darker)return;
+prm_darker=color;emit successLightestChanged();}QColor ThemeManager::getMiscBrown(){return msc_brown;}void ThemeManager::setMiscBrown(QColor color){if(color==msc_brown)
+return;msc_brown=color;emit miscBrownChanged();}
--- gui/thememanager.h
+++ gui/thememanager.h
@@ -17,105 +17,190 @@ enum CurrentTheme : short
 class ThemeManager : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QColor primaryDarkest   READ getPrimaryDarkest)
-    Q_PROPERTY(QColor primaryDarker    READ getPrimaryDarker)
-    Q_PROPERTY(QColor primaryDark      READ getPrimaryDark)
-    Q_PROPERTY(QColor primaryLight     READ getPrimaryLight)
-    Q_PROPERTY(QColor primaryLighter   READ getPrimaryLighter)
-    Q_PROPERTY(QColor primaryLightest  READ getPrimaryLightest)
+    Q_PROPERTY(QColor primaryDarkest   READ getPrimaryDarkest       WRITE setPrimaryDarkest         NOTIFY primaryDarkestChanged) 
+    Q_PROPERTY(QColor primaryDarker    READ getPrimaryDarker        WRITE setPrimaryDarker          NOTIFY primaryDarkerChanged)
+    Q_PROPERTY(QColor primaryDark      READ getPrimaryDark          WRITE setPrimaryDark            NOTIFY primaryDarkChanged)
+    Q_PROPERTY(QColor primaryLight     READ getPrimaryLight         WRITE setPrimaryLight           NOTIFY primaryLightChanged)
+    Q_PROPERTY(QColor primaryLighter   READ getPrimaryLighter       WRITE setPrimaryLighter         NOTIFY primaryLighterChanged)
+    Q_PROPERTY(QColor primaryLightest  READ getPrimaryLightest      WRITE setPrimaryLightest        NOTIFY primaryLightestChanged)
+    
+    Q_PROPERTY(QColor textFaded        READ getTextFaded            WRITE setTextFaded              NOTIFY textFadedChanged)
+    Q_PROPERTY(QColor textColored      READ getTextColored          WRITE setTextColored            NOTIFY textColoredChanged)
+    Q_PROPERTY(QColor textWhite        READ getTextWhite            WRITE setTextWhite              NOTIFY textWhiteChanged)
 
-    Q_PROPERTY(QColor textFaded        READ getTextFaded)
-    Q_PROPERTY(QColor textColored      READ getTextColored)
-    Q_PROPERTY(QColor textWhite        READ getTextWhite)
-
-    Q_PROPERTY(QColor warningDarkest   READ getWarningDarkest)
-    Q_PROPERTY(QColor warningDarker    READ getWarningDarker)
-    Q_PROPERTY(QColor warningDark      READ getWarningDark)
-    Q_PROPERTY(QColor warningLight     READ getWarningLight)
-    Q_PROPERTY(QColor warningLighter   READ getWarningLighter)
-    Q_PROPERTY(QColor warningLightest  READ getWarningLightest)
-
-    Q_PROPERTY(QColor accentDarkest    READ getAccentDarkest)
-    Q_PROPERTY(QColor accentDarker     READ getAccentDarker)
-    Q_PROPERTY(QColor accentDark       READ getAccentDark)
-    Q_PROPERTY(QColor accentLight      READ getAccentLight)
-    Q_PROPERTY(QColor accentLighter    READ getAccentLighter)
-    Q_PROPERTY(QColor accentLightest   READ getAccentLightest)
-
-    Q_PROPERTY(QColor infoDarkest      READ getInfoDarkest)
-    Q_PROPERTY(QColor infoDarker       READ getInfoDarker)
-    Q_PROPERTY(QColor infoDark         READ getInfoDark)
-    Q_PROPERTY(QColor infoLight        READ getInfoLight)
-    Q_PROPERTY(QColor infoLighter      READ getInfoLighter)
-    Q_PROPERTY(QColor infoLightest     READ getInfoLightest)
-
-    Q_PROPERTY(QColor errorDarkest     READ getErrorDarkest)
-    Q_PROPERTY(QColor errorDarker      READ getErrorDarker)
-    Q_PROPERTY(QColor errorDark        READ getErrorDark)
-    Q_PROPERTY(QColor errorLight       READ getErrorLight)
-    Q_PROPERTY(QColor errorLighter     READ getErrorLighter)
-    Q_PROPERTY(QColor errorLightest    READ getErrorLightest)
-
-    Q_PROPERTY(QColor successDarkest   READ getSuccessDarkest)
-    Q_PROPERTY(QColor successDarker    READ getSuccessDarker)
-    Q_PROPERTY(QColor successDark      READ getSuccessDark)
-    Q_PROPERTY(QColor successLight     READ getSuccessLight)
-    Q_PROPERTY(QColor successLighter   READ getSuccessLighter)
-    Q_PROPERTY(QColor successLightest  READ getSuccessLightest)
-
-    Q_PROPERTY(QColor miscBrown        READ getMiscBrown)
+    Q_PROPERTY(QColor warningDarkest   READ getWarningDarkest       WRITE setWarningDarkest         NOTIFY warningDarkestChanged)
+    Q_PROPERTY(QColor warningDarker    READ getWarningDarker        WRITE setWarningDarker          NOTIFY warningDarkerChanged)
+    Q_PROPERTY(QColor warningDark      READ getWarningDark          WRITE setWarningDark            NOTIFY warningDarkChanged)
+    Q_PROPERTY(QColor warningLight     READ getWarningLight         WRITE setWarningLight           NOTIFY warningLightChanged)
+    Q_PROPERTY(QColor warningLighter   READ getWarningLighter       WRITE setWarningLighter         NOTIFY warningLighterChanged)
+    Q_PROPERTY(QColor warningLightest  READ getWarningLightest      WRITE setWarningLightest        NOTIFY warningLightestChanged)
+        
+    Q_PROPERTY(QColor accentDarkest    READ getAccentDarkest        WRITE setAccentDarkest          NOTIFY accentDarkestChanged)
+    Q_PROPERTY(QColor accentDarker     READ getAccentDarker         WRITE setAccentDarker           NOTIFY accentDarkerChanged)
+    Q_PROPERTY(QColor accentDark       READ getAccentDark           WRITE setAccentDark             NOTIFY accentDarkChanged)
+    Q_PROPERTY(QColor accentLight      READ getAccentLight          WRITE setAccentLight            NOTIFY accentLightChanged)
+    Q_PROPERTY(QColor accentLighter    READ getAccentLighter        WRITE setAccentLighter          NOTIFY accentLighterChanged)
+    Q_PROPERTY(QColor accentLightest   READ getAccentLightest       WRITE setAccentLightest         NOTIFY accentLightestChanged)
+    
+    Q_PROPERTY(QColor infoDarkest      READ getInfoDarkest          WRITE setInfoDarkest            NOTIFY infoDarkestChanged)
+    Q_PROPERTY(QColor infoDarker       READ getInfoDarker           WRITE setInfoDarker             NOTIFY infoDarkerChanged)
+    Q_PROPERTY(QColor infoDark         READ getInfoDark             WRITE setInfoDark               NOTIFY infoDarkChanged)
+    Q_PROPERTY(QColor infoLight        READ getInfoLight            WRITE setInfoLight              NOTIFY infoLightChanged)
+    Q_PROPERTY(QColor infoLighter      READ getInfoLighter          WRITE setInfoLighter            NOTIFY infoLighterChanged)
+    Q_PROPERTY(QColor infoLightest     READ getInfoLightest         WRITE setInfoLightest           NOTIFY infoLightestChanged)
+    
+    Q_PROPERTY(QColor errorDarkest     READ getErrorDarkest         WRITE setErrorDarkest           NOTIFY errorDarkestChanged)
+    Q_PROPERTY(QColor errorDarker      READ getErrorDarker          WRITE setErrorDarker            NOTIFY errorDarkerChanged)
+    Q_PROPERTY(QColor errorDark        READ getErrorDark            WRITE setErrorDark              NOTIFY errorDarkChanged)
+    Q_PROPERTY(QColor errorLight       READ getErrorLight           WRITE setErrorLight             NOTIFY errorLightChanged)
+    Q_PROPERTY(QColor errorLighter     READ getErrorLighter         WRITE setErrorLighter           NOTIFY errorLighterChanged)
+    Q_PROPERTY(QColor errorLightest    READ getErrorLightest        WRITE setErrorLightest          NOTIFY errorLightestChanged)
+        
+    Q_PROPERTY(QColor successDarkest   READ getSuccessDarkest       WRITE setSuccessDarkest         NOTIFY successDarkestChanged)
+    Q_PROPERTY(QColor successDarker    READ getSuccessDarker        WRITE setSuccessDarker          NOTIFY successDarkerChanged)
+    Q_PROPERTY(QColor successDark      READ getSuccessDark          WRITE setSuccessDark            NOTIFY successDarkChanged)
+    Q_PROPERTY(QColor successLight     READ getSuccessLight         WRITE setSuccessLight           NOTIFY successLightChanged)
+    Q_PROPERTY(QColor successLighter   READ getSuccessLighter       WRITE setSuccessLighter         NOTIFY successLighterChanged)
+    Q_PROPERTY(QColor successLightest  READ getSuccessLightest      WRITE setSuccessLightest        NOTIFY successLightestChanged)
+        
+    Q_PROPERTY(QColor miscBrown        READ getMiscBrown            WRITE setMiscBrown              NOTIFY miscBrownChanged)
     QML_ELEMENT
 
 public:
     static ThemeManager* get(QObject* parent = nullptr, short themeInitializer = 0);
-
         QColor getPrimaryDarkest();
         QColor getPrimaryDarker();
         QColor getPrimaryDark();
         QColor getPrimaryLight();
         QColor getPrimaryLighter();
         QColor getPrimaryLightest();
-
         QColor getTextFaded();
         QColor getTextColored();
         QColor getTextWhite();
-
         QColor getWarningDarkest();
         QColor getWarningDarker();
         QColor getWarningDark();
         QColor getWarningLight();
         QColor getWarningLighter();
         QColor getWarningLightest();
-
         QColor getAccentDarkest();
         QColor getAccentDarker();
         QColor getAccentDark();
         QColor getAccentLight();
         QColor getAccentLighter();
         QColor getAccentLightest();
-
         QColor getInfoDarkest();
         QColor getInfoDarker();
         QColor getInfoDark();
         QColor getInfoLight();
         QColor getInfoLighter();
         QColor getInfoLightest();
-
         QColor getErrorDarkest();
         QColor getErrorDarker();
         QColor getErrorDark();
         QColor getErrorLight();
         QColor getErrorLighter();
         QColor getErrorLightest();
-
         QColor getSuccessDarkest();
         QColor getSuccessDarker();
         QColor getSuccessDark();
         QColor getSuccessLight();
         QColor getSuccessLighter();
         QColor getSuccessLightest();
-
         QColor getMiscBrown();
+
+
+
+        void setPrimaryDarkest(QColor color);
+        void setPrimaryDarker(QColor color);
+        void setPrimaryDark(QColor color);
+        void setPrimaryLight(QColor color);
+        void setPrimaryLighter(QColor color);
+        void setPrimaryLightest(QColor color);
+        void setTextFaded(QColor color);
+        void setTextColored(QColor color);
+        void setTextWhite(QColor color);
+        void setWarningDarkest(QColor color);
+        void setWarningDarker(QColor color);
+        void setWarningDark(QColor color);
+        void setWarningLight(QColor color);
+        void setWarningLighter(QColor color);
+        void setWarningLightest(QColor color);
+        void setAccentDarkest(QColor color);
+        void setAccentDarker(QColor color);
+        void setAccentDark(QColor color);
+        void setAccentLight(QColor color);
+        void setAccentLighter(QColor color);
+        void setAccentLightest(QColor color);
+        void setInfoDarkest(QColor color);
+        void setInfoDarker(QColor color);
+        void setInfoDark(QColor color);
+        void setInfoLight(QColor color);
+        void setInfoLighter(QColor color);
+        void setInfoLightest(QColor color);
+        void setErrorDarkest(QColor color);
+        void setErrorDarker(QColor color);
+        void setErrorDark(QColor color);
+        void setErrorLight(QColor color);
+        void setErrorLighter(QColor color);
+        void setErrorLightest(QColor color);
+        void setSuccessDarkest(QColor color);
+        void setSuccessDarker(QColor color);
+        void setSuccessDark(QColor color);
+        void setSuccessLight(QColor color);
+        void setSuccessLighter(QColor color);
+        void setSuccessLightest(QColor color);
+        void setMiscBrown(QColor color);
+
+    CurrentTheme currentTheme = CurrentTheme::Swamp;
+
+signals:
+        void primaryDarkestChanged();
+        void primaryDarkerChanged();
+        void primaryDarkChanged();
+        void primaryLightChanged();
+        void primaryLighterChanged();
+        void primaryLightestChanged();
+        void textFadedChanged();
+        void textColoredChanged();
+        void textWhiteChanged();
+        void warningDarkestChanged();
+        void warningDarkerChanged();
+        void warningDarkChanged();
+        void warningLightChanged();
+        void warningLighterChanged();
+        void warningLightestChanged();
+        void accentDarkestChanged();
+        void accentDarkerChanged();
+        void accentDarkChanged();
+        void accentLightChanged();
+        void accentLighterChanged();
+        void accentLightestChanged();
+        void infoDarkestChanged();
+        void infoDarkerChanged();
+        void infoDarkChanged();
+        void infoLightChanged();
+        void infoLighterChanged();
+        void infoLightestChanged();
+        void errorDarkestChanged();
+        void errorDarkerChanged();
+        void errorDarkChanged();
+        void errorLightChanged();
+        void errorLighterChanged();
+        void errorLightestChanged();
+        void successDarkestChanged();
+        void successDarkerChanged();
+        void successDarkChanged();
+        void successLightChanged();
+        void successLighterChanged();
+        void successLightestChanged();
+        void miscBrownChanged();
+
+
+
+private:
+    static ThemeManager* _instance;
+    explicit ThemeManager(QObject *parent = nullptr, short themeInitializer = 0);
 
         QColor prm_darkest;
         QColor prm_darker;
@@ -157,14 +242,6 @@ public:
         QColor scs_lighter;
         QColor scs_lightest;
         QColor msc_brown;
-
-    CurrentTheme currentTheme = CurrentTheme::Swamp;
-
-signals:
-
-private:
-    static ThemeManager* _instance;
-    explicit ThemeManager(QObject *parent = nullptr, short themeInitializer = 0);
 
 };
 

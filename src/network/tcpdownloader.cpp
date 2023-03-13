1f1fc7ecc2a10648741029160a56398223de2170
temp <temp@temp.com>
2023-03-13 14:23:02 +0300

    Backslash n argument fix

--- src/network/tcpdownloader.cpp
+++ src/network/tcpdownloader.cpp
@@ -61,7 +61,7 @@ void TCPDownloader::readFileInfo(QByteArray data)
     readFile = &TCPDownloader::readFileBody;
 
     filename = QString(data);
-    uint8_t i = data.indexOf('\0') + 1;
+    uint8_t i = data.indexOf('\n') + 1;
 
     memcpy(&fileSize, data.mid(i, sizeof(uint32_t)).data(), sizeof(uint32_t));
 

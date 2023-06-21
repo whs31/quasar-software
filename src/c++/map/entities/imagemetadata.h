78a6da4d280ec5eb9697dcb65856b222deaa5d7c
whs31 <75864292+whs31@users.noreply.github.com>
2023-06-21 23:05:40 +0300

    Strip protocol update

--- src/c++/map/entities/imagemetadata.h
+++ src/c++/map/entities/imagemetadata.h
@@ -50,12 +50,12 @@ namespace Map
     //! @brief Заголовок пакета полосового РЛИ.
     struct StripHeaderMetadata
     {
-        uint16_t marker = 0;                    //! <@brief
-        uint16_t version = 0;                   //! <@brief
-        uint16_t size = 0;                      //! <@brief
-        uint16_t cnt = 0;                       //! <@brief
-        uint16_t id = 0;                        //! <@brief
-        uint16_t type = 0;                      //! <@brief
+        uint16_t marker = 0;                    //! <@brief Идентификатор протокола. Равен 0xDEFA.
+        uint16_t version = 0;                   //! <@brief Версия протокола. По маске 0xF000 - мажорная, по 0x0FFF - минорная.
+        uint16_t size = 0;                      //! <@brief Размер пакета в байтах, включая заголовок.
+        uint16_t cnt = 0;                       //! <@brief Счётчик.
+        uint16_t id = 0;                        //! <@brief Идентификатор РЛС. 0x55 - РЛС "Квазар".
+        uint16_t type = 0;                      //! <@brief Идентификатор типа сообщения. 0x1 - полосовое РЛИ. 0x2 - таблица обнаружений.
     } __attribute__((packed));
 
     //! @brief Навигационные данные пакета полосового РЛИ.
@@ -63,9 +63,9 @@ namespace Map
     {
         float pitch = 0;                        //! <@brief Тангаж в градусах.
         float roll = 0;                         //! <@brief Крен в градусах.
-        float elevation = 0;                    //! <@brief Высота БПЛА (в метрах).
-        double latitude = 0;                    //! <@brief
-        double longitude = 0;                   //! <@brief
+        float elevation = 0;                    //! <@brief Высота БПЛА в метрах.
+        double latitude = 0;                    //! <@brief Широта в градусах в проекции WGS-84.
+        double longitude = 0;                   //! <@brief Долгота в градусах в проекции WGS-84.
         float velocity = 0;                     //! <@brief Скорость БПЛА в м/с.
         float course = 0;                       //! <@brief Курс БПЛА в градусах.
         float track_ang = 0;                    //! <@brief Сумма курса БПЛА и угла сноса (в градусах).
@@ -74,16 +74,16 @@ namespace Map
     //! @brief Данные форматирования пакета полосового РЛИ.
     struct StripFormatMetadata
     {
-        float dx = 0;                           //! <@brief
-        float dy = 0;                           //! <@brief
-        float course = 0;                       //! <@brief
-        float roll = 0;                         //! <@brief
-        float x0 = 0;                           //! <@brief
-        uint8_t word_size = 0;                  //! <@brief
-        uint8_t polarization = 0;               //! <@brief
-        int16_t y = 0;                          //! <@brief
-        uint16_t nx = 0;                        //! <@brief
-        uint16_t ny = 0;                        //! <@brief
-        float k = 0;                            //! <@brief
+        float dx = 0;                           //! <@brief Дискрета по дальности в метрах.
+        float dy = 0;                           //! <@brief Дискрета по азимуту в метрах.
+        float course = 0;                       //! <@brief Угол азимута в градусах.
+        float roll = 0;                         //! <@brief Угол наклона в градусах.
+        float x0 = 0;                           //! <@brief Дальность до первого отсчёта в метрах.
+        uint8_t word_size = 0;                  //! <@brief Количество байт в элементе разрешения.
+        uint8_t polarization = 0;               //! <@brief Поляризация: 0 - н/о, 1 - Н/Н, 2 - H/V, 3 - V/H, 4 - V/V, 5 - HV/H, 6 - HV/V.
+        uint16_t y = 0;                         //! <@brief Смещение по Y в метрах.
+        uint16_t nx = 0;                        //! <@brief Общее количество дискрет по дальности.
+        uint16_t ny = 0;                        //! <@brief Общее количество дискрет по азимуту.
+        float k = 0;                            //! <@brief Коэффициент дискреты.
     } __attribute__((packed));
 } // namespace Map;
---
+++ src/qml/widgets/TCPPopup.qml
@@ -0,0 +1,56 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+import QtQuick.Controls.Material 2.15
+import QtQuick.Layouts 1.15
+import Theme 1.0
+
+Pane {
+    property real progress: 0;
+    property bool shown: progress !== 0 && progress !== 100;
+
+    opacity: shown ? 0.9 : 0;
+    Behavior on opacity { NumberAnimation { duration: 300; } }
+    enabled: shown;
+
+    ColumnLayout {
+        spacing: 10;
+
+        Text {
+            text: "Загрузка изображения по TCP-IP";
+            font.family: root.mainfont;
+            font.bold: true;
+            color: Theme.color("light1");
+            font.pixelSize: 15;
+            horizontalAlignment: Text.AlignHCenter;
+            Layout.fillWidth: true;
+        }
+
+        RowLayout {
+            Layout.fillWidth: true;
+            ProgressBar {
+                from: 0;
+                to: 100;
+                value: progress;
+                indeterminate: progress === 0;
+                Material.accent: value === 0 ? Theme.color("light0") : value > 75 ? Theme.color("green")
+                                                                                  : value > 40 ? Theme.color("yellow")
+                                                                                  : Theme.color("red");
+                Layout.fillWidth: true;
+                Layout.fillHeight: true;
+            }
+
+            Text {
+                text: Number(progress).toFixed(0) + "%";
+                font.family: root.mainfont;
+                font.bold: true;
+                color: progress === 0 ? Theme.color("light0") : progress > 75 ? Theme.color("green")
+                                                                              : progress > 40 ? Theme.color("yellow")
+                                                                              : Theme.color("red");
+                font.pixelSize: 14;
+                Layout.fillHeight: true;
+                verticalAlignment: Text.AlignVCenter;
+                horizontalAlignment: Text.AlignHCenter;
+            }
+        }
+    }
+}

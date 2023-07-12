99f5df311337aa268aacf0ab9d8c2bf6b27d5107
whs31 <75864292+whs31@users.noreply.github.com>
2023-07-12 18:15:46 +0300

    Mainpage update

--- docs/modules.js
+++ docs/modules.js
@@ -3,7 +3,7 @@ var modules =
     [ "Ввод и вывод", "dc/d5f/group__sdk__io.html", "dc/d5f/group__sdk__io" ],
     [ "Картография", "d6/d84/group__sdk__map.html", "d6/d84/group__sdk__map" ],
     [ "Сетевое взаимодействие с РЛС", "d3/d54/group__sdk__network.html", "d3/d54/group__sdk__network" ],
-    [ "Датаграммы и сетевые протоколы.", "d8/d07/group__sdk__protocol.html", "d8/d07/group__sdk__protocol" ],
-    [ "Обработка РЛИ.", "d3/daf/group__sdk__images.html", "d3/daf/group__sdk__images" ],
+    [ "Датаграммы и сетевые протоколы", "d8/d07/group__sdk__protocol.html", "d8/d07/group__sdk__protocol" ],
+    [ "Обработка РЛИ", "d3/daf/group__sdk__images.html", "d3/daf/group__sdk__images" ],
     [ "Классы и функции, не рекомендуемые к использованию.", "d4/df9/group__legacy.html", "d4/df9/group__legacy" ]
 ];
